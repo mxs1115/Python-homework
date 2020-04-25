8e4e50f7ded8a2f3dcccbf0ecbe09702b6bae587
mxs1115 <1926096363@qq.com>
2020-04-26 01:07:07 +0800

    第七次作业

--- homework7/4/4.py
+++ homework7/4/4.py
@@ -19,56 +19,20 @@ for i in re_a:
    list.append(i.attrs['href'])
 print(list)
 
-"""
-	2、根据获取的每个练习的链接地址来请求每个练习获得页面内容
-"""
-#遍历列表100次
+
 data = []
 os.chdir(r"homework7\4")
 for x in list:
-	# 请求详细页面
 	test = requests.get(x, headers=headers).content.decode('utf-8')
-	print(x)
-	# print(test)
+	#print(x)
 	
-	# 解析html文档
 	soup_test = BeautifulSoup(test, 'html.parser')
 	title = soup_test.find(class_="page__title").text
-	print(soup_test.find(class_="page__title").text)
+	#print(soup_test.find(class_="page__title").text)
 	tm = soup_test.find(class_="content").text
 	with open('text-py.txt','a+',encoding='utf-8') as f:
 		f.write(title+'\n')
 		f.write(tm+'\n')
 		f.write('*'*50+'\n')
 		f.write('\n')
-	
-       
-	# 查找程序分析
-	#dict['cxfx'] = soup_test.find(id='content').find_all('p')[2].text
-	# print(cxfx)
-	
-	# 程序源代码
-	#try:
-	#	dict['code'] = soup_test.find(class_="hl-main").text
-	#except Exception as e:
-		#dict['code'] = soup_test.find('pre').text
-	# print(code)
-	# print(dict)
-	
-#保存文件1
-# 	data.append(dict)
-#
-# import pandas as pd
-# datas = pd.DataFrame(data)
-# datas.to_csv('py-100.csv')
- 
-#保存文件2
-    
-
-#with open('text-py.txt','a+',encoding='utf-8') as file:
-    
-    
-    #file.write(dict['cxfx']+'\n')
-    #file.write(dict['code']+'\n')
-    
     
